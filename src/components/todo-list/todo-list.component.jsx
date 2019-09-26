479c34fd8c417bc123e0326dd6ea899206881fcf
Devvrat Shukla <devvratshukla@gmail.com>
2019-09-26 16:18:05 +0530

    feat(component): Added TodoList component

---
+++ src/components/todo-list/todo-list.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Todo from './../todo/todo.component';
+
+const TodoList = ({ toDos, onTodoClick }) => {
+    return (
+        <ul>
+            {toDos.map((todo, index) => (
+                <Todo key={index} onClick={() => onTodoClick(index)} {...todo} />
+            ))}
+        </ul>
+    );
+};
+
+TodoList.propTypes = {
+    toDos: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            completed: PropTypes.bool.isRequired,
+            text: PropTypes.string.isRequired
+        }).isRequired
+    ).isRequired,
+    onTodoClick: PropTypes.func.isRequired
+};
+
+export default TodoList;
